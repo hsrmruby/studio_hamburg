c6c4f316686429f473b9555dea205a9c55afff78
hsrmruby <Philipp_Herrnberger@gmx.de>
2011-10-18 12:50:51 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudioHamburg::Application.config.secret_token = '754cbc712beab9d656a7995df157b952149906d00dd725ca2344ab8869fe363af3783d20650b48d875670007cfbc951ce68286f7fda83a5a97dfc9b0cab20b6d'
